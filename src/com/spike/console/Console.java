9cabe2e689813d9ca2061677f611781843c8111b
Ryan Mcgilvery <ss6414403@gmail.com>
2016-10-20 09:50:00 -0500

    neatting up some code

--- src/com/spike/console/Console.java
+++ src/com/spike/console/Console.java
@@ -3,36 +3,44 @@ package com.spike.console;
 import com.spike.main;
 import com.spike.screen.Screen;
 
-public class Console{
+public class Console {
 	private Screen s = main.getScreen();
-	//private static char[][] console = new char[s.width][s.height];
+	// private static char[][] console = new char[s.width][s.height];
 	public Cursor cursor = new Cursor();
 	private boolean LineWrap = true;
-	
-	public void print(String line){
+
+	public void print(String line) {
 		int x = this.cursor.x;
-		for(int i = 0;i<line.length();i++){
-			boolean end = this.s.setPixel(i+x, this.cursor.y, line.charAt(i));
-			if(end){
-				this.cursor.x++;
-			}else{
-				if(LineWrap)
-					break;
+		for (int i = 0; i < line.length(); i++) {
+			if (line.charAt(i) != '\n') {
+				boolean end = this.s.setPixel(i + x, this.cursor.y, line.charAt(i));
+				if (end) {
+					this.cursor.x++;
+				} else {
+					if (LineWrap)
+						break;
+					this.cursor.y++;
+					this.cursor.x = 0;
+					x = -i;
+					this.s.setPixel(0, this.cursor.y, line.charAt(i));
+				}
+			} else {
 				this.cursor.y++;
 				this.cursor.x=0;
-				x=-i;
-				this.s.setPixel(0, this.cursor.y, line.charAt(i));
+				x = -i-1;
 			}
-			
+
 		}
 		s.render();
 	}
-	public void printAtPos(int x,int y,String line){
+
+	public void printAtPos(int x, int y, String line) {
 		this.cursor.setPos(x, y);
-		
+
 		this.print(line);
 	}
-	public void lineWrap(boolean lw){
+
+	public void lineWrap(boolean lw) {
 		this.LineWrap = lw;
 	}
 
--- src/com/spike/screen/Screen.java
+++ src/com/spike/screen/Screen.java
@@ -2,40 +2,45 @@ package com.spike.screen;
 
 public class Screen {
 	public char[][] Screen;
-	public int width,height;
-	public Screen(){
+	public int width, height;
+
+	public Screen() {
 		Screen = new char[10][10];
 		width = 10;
 		height = 10;
 		clear();
 	}
-	public Screen(int w,int h){
+
+	public Screen(int w, int h) {
 		Screen = new char[w][h];
 		width = w;
 		height = h;
 		clear();
 	}
-	public void clear(){
-		for(int x = 0;x<width;x++){
-			for(int y = 0;y<height;y++){
+
+	public void clear() {
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
 				Screen[x][y] = ' ';
 			}
 		}
 	}
-	public boolean setPixel(int x,int y,char c){
-		if(x>=this.width || y>=this.height || x<0 || y<0)
+
+	public boolean setPixel(int x, int y, char c) {
+		if (x >= this.width || y >= this.height || x < 0 || y < 0)
 			return false;
 		Screen[x][y] = c;
 		return true;
 	}
-	public void render(){
+
+	public void render() {
 		System.out.println("\n\n\n\n");
-		for(int i =0;i<this.width;i++){
+		for (int i = 0; i < this.width; i++) {
 			System.out.print("-");
 		}
 		System.out.print("\n");
-		for(int y = 0;y<height;y++){
-			for(int x = 0;x<width;x++){
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
 				System.out.write(Screen[x][y]);
 			}
 			System.out.print("\n");
